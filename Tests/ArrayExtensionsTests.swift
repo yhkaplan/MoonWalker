900cde8729fe6f94dabf3a7a068208799772546d
Joshua Kaplan <yhkaplan@gmail.com>
2018-06-30 16:11:08 +0900

    Added more tests to ArrayExtensionsTests

--- Tests/ArrayExtensionsTests.swift
+++ Tests/ArrayExtensionsTests.swift
@@ -43,6 +43,55 @@ class ArrayExtensionsTests: QuickSpec {
                     
                     expect(tested).to(equal(expected))
                 }
+
+                it("returns nil for empty array") {
+                    
+                    let arrayIndex = 0
+                    let emptyArray: [Int] = []
+                    
+                    let tested: Int? = emptyArray[after: arrayIndex]
+                    
+                    expect(tested).to(beNil())
+                }
+                
+            }
+
+            context("When subscript before is called") {
+                
+                it("returns the last value for startIndex") {
+
+                    guard
+                        let expected = array.last,
+                        let tested = array[before: array.startIndex]
+                    else {
+                            fail(); return
+                    }
+
+                    expect(tested).to(equal(expected))
+                }
+
+                it("returns the value after for regular index") {
+
+                    let arrayIndex = 1
+
+                    let expected = array[arrayIndex - 1]
+
+                    guard let tested = array[before: arrayIndex] else {
+                        fail(); return
+                    }
+
+                    expect(tested).to(equal(expected))
+                }
+                
+                it("returns nil for empty array") {
+                    
+                    let arrayIndex = 0
+                    let emptyArray: [Int] = []
+                    
+                    let tested: Int? = emptyArray[before: arrayIndex]
+                    
+                    expect(tested).to(beNil())
+                }
                 
             }
             
