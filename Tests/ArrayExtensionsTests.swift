a644dafdbee54f866a063dac303cf01683154492
Joshua Kaplan <yhkaplan@gmail.com>
2018-06-30 16:02:55 +0900

    Added tests for subscript[after:]

--- Tests/ArrayExtensionsTests.swift
+++ Tests/ArrayExtensionsTests.swift
@@ -5,4 +5,48 @@
 //  Created by Joshua Kaplan on 2018/06/30.
 //
 
-import Foundation
+import Quick
+import Nimble
+@testable import MoonWalker
+
+class ArrayExtensionsTests: QuickSpec {
+    
+    override func spec() {
+        
+        describe("Subscript funcs") {
+            
+            let array: [String] = ["green", "blue", "red"]
+            
+            context("When subscript after is called") {
+                
+                it("returns the first value for endIndex") {
+                    
+                    guard
+                        let expected = array.first,
+                        let tested = array[after: array.endIndex]
+                    else {
+                        fail(); return
+                    }
+                    
+                    expect(tested).to(equal(expected))
+                }
+             
+                it("returns the value after for regular index") {
+                    
+                    let arrayIndex = 1
+                    
+                    let expected = array[arrayIndex + 1]
+                    
+                    guard let tested = array[after: arrayIndex] else {
+                        fail(); return
+                    }
+                    
+                    expect(tested).to(equal(expected))
+                }
+                
+            }
+            
+        }
+        
+    }
+}
