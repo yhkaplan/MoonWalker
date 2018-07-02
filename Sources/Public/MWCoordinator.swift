a8edfc388a2ece30aba731932d7a208cb070c3de
yhkaplan <yhkaplan@gmail.com>
2018-07-02 16:47:56 +0900

    Moved pageVCConfigurator to its own file

--- Sources/Public/MWCoordinator.swift
+++ Sources/Public/MWCoordinator.swift
@@ -58,22 +58,3 @@ public extension MWParentViewModel {
     }
 
 }
-
-// This struct is resposible for configuring UIPageViewController
-struct PageVCConfigurator {
-
-    let pageViewController: UIPageViewController
-
-    func setDataSource(_ dataSource: UIPageViewControllerDataSource?) {
-        pageViewController.dataSource = dataSource
-    }
-
-    func setInitialChildViewController(to initiallyVisableVC: ChildViewController) {
-        pageViewController.setViewControllers(
-            [initiallyVisableVC],
-            direction: .forward,
-            animated: true
-        )
-    }
-
-}
