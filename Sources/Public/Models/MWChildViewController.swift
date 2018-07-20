faef577cd3d73247a7aabbdcb4f40d2afc04f961
yhkaplan <yhkaplan@gmail.com>
2018-07-20 16:07:00 +0900

    Move ChildView to new file

---
+++ Sources/Public/Models/MWChildViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MWChildViewController.swift
+//  MoonWalker
+//
+//  Created by josh on 2018/07/20.
+//
+
+import UIKit
+
+public struct ChildView: Equatable {
+    public var viewController: UIViewController?
+    public var layout: CenteredObjectLayout
+
+    public init(
+        viewController: UIViewController? = nil,
+        layout: CenteredObjectLayout = CenteredObjectLayout()
+        ) {
+        self.viewController = viewController
+        self.layout = layout
+    }
+
+    func embed(in parentVC: UIViewController) {
+        guard let viewController = viewController else { return }
+
+        parentVC.embed(
+            childVC: viewController,
+            leadingConstant: layout.leadingConstant,
+            trailingConstant: layout.trailingConstant,
+            topConstant: layout.topConstant,
+            bottomConstant: layout.bottomConstant
+        )
+    }
+}
--- Sources/Public/Models/MWChildViewLayoutSettings.swift
+++ Sources/Public/Models/MWChildViewLayoutSettings.swift
@@ -49,27 +49,3 @@ public struct CenteredObjectLayout: RegularLayout, SubviewAddable, Equatable {
     }
 }
 
-public struct ChildView: Equatable {
-    public var viewController: UIViewController?
-    public var layout: CenteredObjectLayout
-
-    public init(
-        viewController: UIViewController? = nil,
-        layout: CenteredObjectLayout = CenteredObjectLayout()
-    ) {
-        self.viewController = viewController
-        self.layout = layout
-    }
-
-    func embed(in parentVC: UIViewController) {
-        guard let viewController = viewController else { return }
-
-        parentVC.embed(
-            childVC: viewController,
-            leadingConstant: layout.leadingConstant,
-            trailingConstant: layout.trailingConstant,
-            topConstant: layout.topConstant,
-            bottomConstant: layout.bottomConstant
-        )
-    }
-}
