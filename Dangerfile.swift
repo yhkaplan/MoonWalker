52431d4560dffe9b8f88856b5db40b357100e5a7
Joshua Kaplan <yhkaplan@gmail.com>
2018-07-21 23:08:09 +0900

    Give danger-swiftlint another try

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,7 +1,11 @@
-import Danger
+// Dangerfile.swift
 
+import Danger
+import DangerSwiftLint // package: https://github.com/ashfurrow/danger-swiftlint.git
 let danger = Danger()
 
 if danger.git.createdFiles.count + danger.git.modifiedFiles.count - danger.git.deletedFiles.count > 10 {
     warn("Big PR, try to keep changes smaller if you can")
 }
+
+SwiftLint.lint(inline: true, directory: "Sources", configFile: ".swiftlint.yml")
