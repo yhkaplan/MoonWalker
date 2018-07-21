426de95abc6b59e197a0acfb730a53fdb8a728a5
Joshua Kaplan <yhkaplan@gmail.com>
2018-07-21 17:17:39 +0900

    Setup Swiftlint-danger

---
+++ Dangerfile.swift
@@ -0,0 +1,5 @@
+import Danger
+import DangerSwiftLint // package: https://github.com/ashfurrow/DangerSwiftLint.git
+
+let danger = Danger()
+SwiftLint.lint(inline: true, directory: "Sources", configFile: ".swiftlint.autocorrect.yml")
