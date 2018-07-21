e156c448b78a16bd3e9bdbe8bb7fd6736c5f5afd
Joshua Kaplan <yhkaplan@gmail.com>
2018-07-21 18:44:19 +0900

    Test failing for Travis: to revert!

--- Tests/Internal/Controllers/ChildViewControllerTests.swift
+++ Tests/Internal/Controllers/ChildViewControllerTests.swift
@@ -132,7 +132,7 @@ class ChildViewControllerTests: QuickSpec {
                 }
 
                 it("mainImage image is set") {
-                    expect(childViewController.hasImage(with: mainUIImage)).to(beTrue())
+                    expect(childViewController.hasImage(with: mainUIImage)).to(beFalse())
                 }
 
                 it("backgroundImage image is set") {
