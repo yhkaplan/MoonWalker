7b12e8537b983c3422b7107683ce4c180f48b0f1
Joshua Kaplan <yhkaplan@gmail.com>
2018-07-21 22:04:29 +0900

    Try deleting Danger-Swiftlint

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,5 +1,7 @@
 import Danger
-import DangerSwiftLint // package: git@github.com:ashfurrow/danger-swiftlint.git
 
 let danger = Danger()
-SwiftLint.lint(inline: true, directory: "Sources", configFile: ".swiftlint.yml")
+
+if danger.git.createdFiles.count + danger.git.modifiedFiles.count - danger.git.deletedFiles.count > 10 {
+    warn("Big PR, try to keep changes smaller if you can")
+}
