a043486d5f0bd7e8e2f5cca39d15e60175038371
yhkaplan <yhkaplan@gmail.com>
2018-07-23 11:32:48 +0900

    Remove dangerswiftlint code for now

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,6 +1,5 @@
 import Foundation
 import Danger
-import DangerSwiftLint // package: https://github.com/ashfurrow/danger-swiftlint.git
 
 let danger = Danger()
 
@@ -8,5 +7,3 @@ if danger.git.createdFiles.count + danger.git.modifiedFiles.count - danger.git.d
     warn("Big PR, try to keep changes smaller if you can")
 }
 
-// No dangerswiftlint for now
-// SwiftLint.lint(inline: true, directory: "Tests", configFile: ".swiftlint.yml")
