d5a0db9d27cba2faa4c0991af4c88a74e8b622cf
yhkaplan <yhkaplan@gmail.com>
2018-07-23 11:12:25 +0900

    Turn off prefixed_toplevel_constant and deactivate danger-swiftlint for now

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,10 +1,12 @@
 import Foundation
 import Danger
 import DangerSwiftLint // package: https://github.com/ashfurrow/danger-swiftlint.git
+
 let danger = Danger()
 
 if danger.git.createdFiles.count + danger.git.modifiedFiles.count - danger.git.deletedFiles.count > 10 {
     warn("Big PR, try to keep changes smaller if you can")
 }
 
-SwiftLint.lint(inline: true, directory: "Tests", configFile: ".swiftlint.yml")
+// No dangerswiftlint for now
+// SwiftLint.lint(inline: true, directory: "Tests", configFile: ".swiftlint.yml")
