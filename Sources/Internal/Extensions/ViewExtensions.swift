91f1f0d45f1115e29ecd414e8131b05f01e12592
yhkaplan <yhkaplan@gmail.com>
2018-07-26 18:59:38 +0900

    Write lots of ugly code for safeAreaLayoutGuide

--- Sources/Internal/Extensions/ViewExtensions.swift
+++ Sources/Internal/Extensions/ViewExtensions.swift
@@ -9,21 +9,51 @@ import UIKit
 
 extension UIView {
 
+    private func setHeightConstraint(_ height: CGFloat, for subView: UIView) {
+        subView.heightAnchor.constraint(equalToConstant: height)
+    }
+
     func addSubviewWithConstraints(
         _ subView: UIView,
         leadingConstant: CGFloat = 0.0,
         trailingConstant: CGFloat = 0.0,
         topConstant: CGFloat = 0.0,
-        bottomConstant: CGFloat = 0.0
+        bottomConstant: CGFloat = 0.0,
+        withSafeArea: Bool = false
     ) {
         addSubview(subView)
         subView.translatesAutoresizingMaskIntoConstraints = false
 
-        [subView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: leadingConstant),
-         subView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: trailingConstant),
-         subView.topAnchor.constraint(equalTo: topAnchor, constant: topConstant),
-         subView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: bottomConstant)
-        ].forEach { $0.isActive = true }
+        var constraints: [NSLayoutConstraint] = []
+
+        //TODO: optimize this w/ wrapper func
+        if #available(iOS 11.0, *) {
+            if withSafeArea {
+                constraints = [
+                    subView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: leadingConstant),
+                    subView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: trailingConstant),
+                    subView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: topConstant),
+                    subView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: bottomConstant)
+                ]
+            } else {
+            constraints = [
+                subView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: leadingConstant),
+                subView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: trailingConstant),
+                subView.topAnchor.constraint(equalTo: topAnchor, constant: topConstant),
+                subView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: bottomConstant)
+            ]
+
+            }
+        } else {
+            constraints = [
+                subView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: leadingConstant),
+                subView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: trailingConstant),
+                subView.topAnchor.constraint(equalTo: topAnchor, constant: topConstant),
+                subView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: bottomConstant)
+            ]
+        }
+
+        NSLayoutConstraint.activate(constraints)
     }
 
     func addSubviewWithConstraints(
@@ -36,11 +66,25 @@ extension UIView {
         addSubview(subView)
         subView.translatesAutoresizingMaskIntoConstraints = false
 
-        [subView.heightAnchor.constraint(equalToConstant: height),
-         subView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: leadingConstant),
-         subView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: trailingConstant),
-         subView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: bottomConstant)
-        ].forEach { $0.isActive = true }
+        var constraints: [NSLayoutConstraint] = []
+
+        if #available(iOS 11.0, *) {
+            constraints = [
+                subView.safeAreaLayoutGuide.heightAnchor.constraint(equalToConstant: height),
+                subView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: leadingConstant),
+                subView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: trailingConstant),
+                subView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: bottomConstant)
+            ]
+        } else {
+            constraints = [
+                subView.heightAnchor.constraint(equalToConstant: height),
+                subView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: leadingConstant),
+                subView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: trailingConstant),
+                subView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: bottomConstant)
+            ]
+        }
+
+        NSLayoutConstraint.activate(constraints)
     }
 
     func addSubviewWithConstraints(
@@ -53,11 +97,25 @@ extension UIView {
         addSubview(subView)
         subView.translatesAutoresizingMaskIntoConstraints = false
 
-        [subView.heightAnchor.constraint(equalToConstant: height),
-         subView.widthAnchor.constraint(equalToConstant: width),
-         subView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: leadingConstant),
-         subView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: bottomConstant)
-        ].forEach { $0.isActive = true }
+        var constraints: [NSLayoutConstraint] = []
+
+        if #available(iOS 11.0, *) {
+            constraints = [
+                subView.safeAreaLayoutGuide.heightAnchor.constraint(equalToConstant: height),
+                subView.safeAreaLayoutGuide.widthAnchor.constraint(equalToConstant: width),
+                subView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: leadingConstant),
+                subView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: bottomConstant)
+            ]
+        } else {
+            constraints = [
+                subView.heightAnchor.constraint(equalToConstant: height),
+                subView.widthAnchor.constraint(equalToConstant: width),
+                subView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: leadingConstant),
+                subView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: bottomConstant)
+            ]
+        }
+
+        NSLayoutConstraint.activate(constraints)
     }
 
     func addSubviewWithConstraints(
@@ -70,11 +128,25 @@ extension UIView {
         addSubview(subView)
         subView.translatesAutoresizingMaskIntoConstraints = false
 
-        [subView.heightAnchor.constraint(equalToConstant: height),
-         subView.widthAnchor.constraint(equalToConstant: width),
-         subView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: trailingConstant),
-         subView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: bottomConstant)
-        ].forEach { $0.isActive = true }
+        var constraints: [NSLayoutConstraint] = []
+
+        if #available(iOS 11.0, *) {
+            constraints = [
+                subView.safeAreaLayoutGuide.heightAnchor.constraint(equalToConstant: height),
+                subView.safeAreaLayoutGuide.widthAnchor.constraint(equalToConstant: width),
+                subView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: trailingConstant),
+                subView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: bottomConstant)
+            ]
+        } else {
+            constraints = [
+                subView.heightAnchor.constraint(equalToConstant: height),
+                subView.widthAnchor.constraint(equalToConstant: width),
+                subView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: trailingConstant),
+                subView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: bottomConstant)
+            ]
+        }
+
+        NSLayoutConstraint.activate(constraints)
     }
 
     func addSubviewWithConstraints(
@@ -87,11 +159,25 @@ extension UIView {
         addSubview(subView)
         subView.translatesAutoresizingMaskIntoConstraints = false
 
-        [subView.heightAnchor.constraint(equalToConstant: height),
-         subView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: leadingConstant),
-         subView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: trailingConstant),
-         subView.topAnchor.constraint(equalTo: topAnchor, constant: topConstant)
-        ].forEach { $0.isActive = true }
+        var constraints: [NSLayoutConstraint] = []
+
+        if #available(iOS 11.0, *) {
+            constraints = [
+                subView.safeAreaLayoutGuide.heightAnchor.constraint(equalToConstant: height),
+                subView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: leadingConstant),
+                subView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: trailingConstant),
+                subView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: topConstant)
+            ]
+        } else {
+            constraints = [
+                subView.heightAnchor.constraint(equalToConstant: height),
+                subView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: leadingConstant),
+                subView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: trailingConstant),
+                subView.topAnchor.constraint(equalTo: topAnchor, constant: topConstant)
+            ]
+        }
+
+        NSLayoutConstraint.activate(constraints)
     }
 
     func addSubviewWithConstraints(
@@ -102,9 +188,21 @@ extension UIView {
         addSubview(subView)
         subView.translatesAutoresizingMaskIntoConstraints = false
 
-        [subView.centerXAnchor.constraint(equalTo: centerXAnchor, constant: centerXConstant),
-         subView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: bottomConstant)
-        ].forEach { $0.isActive = true }
+        var constraints: [NSLayoutConstraint] = []
+
+        if #available(iOS 11.0, *) {
+            constraints = [
+                subView.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor, constant: centerXConstant),
+                subView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: bottomConstant)
+            ]
+        } else {
+            constraints = [
+                subView.centerXAnchor.constraint(equalTo: centerXAnchor, constant: centerXConstant),
+                subView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: bottomConstant)
+            ]
+        }
+
+        NSLayoutConstraint.activate(constraints)
     }
 
 }
